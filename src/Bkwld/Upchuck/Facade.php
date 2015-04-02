edb2283f127fcb8c590994c80c1cc969828fb98d
Robert Reinhard <robert.reinhard@bkwld.com>
2015-04-02 14:46:27 -0700

    Adding a facade for disk operations
    For the most aprt

---
+++ src/Bkwld/Upchuck/Facade.php
@@ -0,0 +1,28 @@
+<?php namespace Bkwld\Upchuck;
+
+// Dependencies
+use Config;
+
+class Facade extends \Illuminate\Support\Facades\Facade {
+
+	/**
+	 * Get a URL given the path to an asset uploaded via Upchuck
+	 *
+	 * @param string $path 
+	 * @return string
+	 */
+	public function url($path) {
+		return static::$app['upchuck']->url($path);
+	}
+
+	/**
+	 * Return the Flysystem remote disk as the main facade so
+	 * the remote disk can be easily interacted with
+	 * 
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { 
+		return 'upchuck.disk'; 
+	}
+
+}
